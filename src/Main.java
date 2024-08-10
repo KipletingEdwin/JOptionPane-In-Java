9f18316411f17ff58f13c8d3fbd3564abe6dcbbc
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-10 16:48:48 +0300

    add showConfirmDialog

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,13 @@ public class Main {
         //JOptionPane =  pop up a standard dialog box that prompts the user for a value
          //                or informs them of something
 
-        JOptionPane.showMessageDialog(null,"This is some useless info", "title",JOptionPane.PLAIN_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"This is some useless info", "title",JOptionPane.PLAIN_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"This is some useless info", "title",JOptionPane.INFORMATION_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"This is some useless info", "title",JOptionPane.QUESTION_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"This is some useless info", "title",JOptionPane.WARNING_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"This is some useless info", "title",JOptionPane.ERROR_MESSAGE);
+
+        JOptionPane.showConfirmDialog(null,"Bro,do you even code?","This is my title",JOptionPane.YES_NO_CANCEL_OPTION);
 
     }
 }
